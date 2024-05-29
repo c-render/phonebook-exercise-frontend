f0e2e70b79267932fd0d5bc2ba2599b040ecfc02
cscharnhorst <cscharnhorst@microsoft.com>
2024-05-29 11:01:02 +0200

    Changed vite.config.js to still support "npm run dev"

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // const baseUrl = 'http://localhost:3001/api/persons'
-const baseUrl = 'https://phonebook-exercise-apmp.onrender.com/api/persons'
+//const baseUrl = 'https://phonebook-exercise-apmp.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
